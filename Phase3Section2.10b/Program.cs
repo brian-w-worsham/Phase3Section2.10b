5dcf355a3f82663b0422edd279da09d3c1ec3712
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-06 22:10:08 -0500

    Add project files.

---
+++ Phase3Section2.10b/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Phase3Section2._10b.Models;
+
+namespace Phase3Section2._10b
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            string? connectionString = builder.Configuration.GetConnectionString("SchoolConnectionString");
+
+            builder.Services.AddDbContext<SchoolEntities>(options =>
+                           options.UseSqlServer(connectionString));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
